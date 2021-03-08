f478629ee2b4e9e08bdcc57bf8871de588e06ea2
vitalii43 <2kn15b.bondarchuk@gmail.com>
2021-03-08 22:31:38 +0200

    css-modules: refactored

--- movies-app/src/components/MoviesList/MoviesList.js
+++ movies-app/src/components/MoviesList/MoviesList.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import styles from './MoviesList.scss';
+
+import {classNames} from '../../utils';
+import styles from './MoviesList.module.scss';
 import {MovieCard} from '../MovieCard';
 import PropTypes from 'prop-types';
 
+const cn = classNames(styles);
+
 export const MoviesList = ({movies}) => {
   return (
-    <div className="movies-list-container">
-      <nav className="movies-navbar">
+    <div className={cn("movies-list-container")}>
+      <nav className={cn("movies-navbar")}>
         <ul className="nav">
           <li className="nav__item">all</li>
           <li className="nav__item active">documentary</li>
@@ -14,22 +18,22 @@ export const MoviesList = ({movies}) => {
           <li className="nav__item">horror</li>
           <li className="nav__item">Crime</li>
         </ul>
-        <div className="movies-sort">
-          <label className="movies-sort__label">SORT BY</label>
-          <select className="movies-sort__select" name="sortby" defaultValue="release">
+        <div className={cn("movies-sort")}>
+          <label className={cn("movies-sort__label")}>SORT BY</label>
+          <select className={cn("movies-sort__select")} name="sortby" defaultValue="release">
             <option value="release">RELEASE DATE</option>
             <option value="ganre" >GANRE</option>
           </select>
         </div>
       </nav>
 
-      <div className="result-count-container">
-        <span className="result-count">{movies.list.length}</span> movies found
+      <div className={cn("result-count-container")}>
+        <span className={cn("result-count")}>{movies.list.length}</span> movies found
       </div>
 
-      <div className="movie-card-list">
+      <div className={cn("movie-card-list")}>
         {movies.list.map((movie) => (
-          <div className="movie-card" key={movie.id}>
+          <div className={cn("movie-card")} key={movie.id}>
             <MovieCard movie={movie} />
           </div>
         ))}
---
+++ movies-app/src/utils/index.js
@@ -0,0 +1,5 @@
+import classNames from './styles';
+
+export {
+  classNames
+}
